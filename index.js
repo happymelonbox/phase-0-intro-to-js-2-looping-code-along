b1fac35bd3b24ae87563e0b7b00cb25751a5903b
Justin Berger Howes <howes.j.j@gmail.com>
2021-05-03 00:04:46 +1000

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function countDown(int){
+    while (int >= 0) {
+        console.log(int--)}};
+
+
+        let messages = [];
+
+        function writeCards(names, event) {
+            for (let i = 0; i < names.length; i++ ) {
+                messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+            }
+            return messages;
+        }
